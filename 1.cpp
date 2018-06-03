fd00f79d452da9a9cc3ff5a56a6d7da6470b941a
AiTgier <39871417+AiTgier@users.noreply.github.com>
2018-06-03 13:19:22 +0800

    add

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>;
+
+using namespace std;
+int main()
+	{
+	int n,i;
+
+	double result=0;
+	cout<<"下面将进行阶乘运算n!\n请输入n的值\n";
+	cin>>n;
+	if(n<1)
+	{
+		cout<<"输入错误！"<<endl;
+		return 1;
+	}
+	result=1;
+	for (i=2;i<=n;i++)result*=i;
+	cout<<result<<endl;
+	return 0;
+}
